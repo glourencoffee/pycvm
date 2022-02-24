1488e76c8940aad0609da7adb0e693f289cfff07
Giovanni L <callmegiorgio@hotmail.com>
2022-02-24 17:07:06 -0300

    created unit tests

---
+++ tests/test_bpa.py
@@ -0,0 +1,42 @@
+import unittest
+from cvm.parsing.dfp.bpa import IndustrialCompanyBalance
+
+class TestBPA(unittest.TestCase):
+    def test_industrial(self):
+        # BPA/2010 of CENTRAIS ELET BRAS S.A. - ELETROBRAS (ELET)
+        accounts = [
+            ('1',       'Ativo Total',                    146901002),
+            ('1.01',    'Ativo Circulante',               32805947),
+            ('1.01.01', 'Caixa e Equivalentes de Caixa',  11278387),
+            ('1.01.02', 'Aplicações Financeiras',         6774073),
+            ('1.01.03', 'Contas a Receber',               4016006),
+            ('1.01.04', 'Estoques',                       676609),
+            ('1.01.05', 'Ativos Biológicos',              0),
+            ('1.01.06', 'Tributos a Recuperar',           0),
+            ('1.01.07', 'Despesas Antecipadas',           40418),
+            ('1.01.08', 'Outros Ativos Circulantes',      10020454),
+            ('1.02',    'Ativo Não Circulante',           114095055),
+            ('1.02.01', 'Ativo Realizável a Longo Prazo', 60423937),
+            ('1.02.02', 'Investimentos',                  4724648),
+            ('1.02.03', 'Imobilizado',                    46682498),
+            ('1.02.04', 'Intangível',                     2263972)
+        ]
+
+        balance = IndustrialCompanyBalance(accounts)
+        balance.validate()
+
+        self.assertEquals(balance.total_assets,              146901002)
+        self.assertEquals(balance.current_assets,            32805947)
+        self.assertEquals(balance.cash_and_cash_equivalents, 11278387)
+        self.assertEquals(balance.financial_investments,     6774073)
+        self.assertEquals(balance.accounts_receivable,       4016006)
+        self.assertEquals(balance.inventories,               676609)
+        self.assertEquals(balance.biological_assets,         0)
+        self.assertEquals(balance.taxes_recoverable,         0)
+        self.assertEquals(balance.prepaid_expenses,          40418)
+        self.assertEquals(balance.other_current_assets,      10020454)
+        self.assertEquals(balance.noncurrent_assets,         114095055)
+        self.assertEquals(balance.long_term_assets,          60423937)
+        self.assertEquals(balance.long_term_investiments,    4724648)
+        self.assertEquals(balance.fixed_assets,              46682498)
+        self.assertEquals(balance.intangible_assets,         2263972)
