612ae3d3a31e740030a9b921467dc6fc4839a75d
Giovanni L <callmegiorgio@hotmail.com>
2022-09-21 09:58:30 -0300

    Refactor: Use versioneer for versioning (#8)

--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+import versioneer
 
-setup(
-    name='cvm',
-    version='0.2.0',
-    packages=find_packages(include=['cvm'])
+with open('README_en_US.md') as f:
+    readme_en_us = f.read()
+
+setup(name                          = 'CVM',
+      version                       = versioneer.get_version(),
+      cmdclass                      = versioneer.get_cmdclass(),
+      description                   = 'Python library for processing data from CVM',
+      long_description              = readme_en_us,
+      long_description_content_type = 'text/markdown',
+      author                        = 'Giovanni L',
+      author_email                  = 'callmegiorgio@hotmail.com',
+      url                           = 'https://github.com/callmegiorgio/cvm/',
+      license                       = 'MIT',
+      packages                      = ['cvm'],
+      keywords                      = ['investment', 'finances'],
+      install_requires              = [],
+      python_requires               = '>=3.7'
 )
